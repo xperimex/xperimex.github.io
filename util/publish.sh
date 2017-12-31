55fbb8b33fa1f695179d811f0c66cac72f9a2626
gmittal <gautam@mittal.net>
2017-12-30 16:34:04 -0800

    Sat Dec 30 16:34:04 PST 2017

--- util/publish.sh
+++ util/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+npm run build
 git add .
 git commit -m "$(date)"
 git push origin master
--- util/test.sh
+++ util/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+npm run build
 git add .
 git commit -m "$(date)"
 python -m SimpleHTTPServer
